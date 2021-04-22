ad218ab6a446dd2bc61b3fbaf02c6e2d0eeb5359
TylerCambronGH <77807623+TylerCambronGH@users.noreply.github.com>
2021-04-22 15:29:56 -0400

    Delete src/tc/pp3 directory

--- src/tc/pp3/interfaces/Swimmable.java
+++
@@ -1,9 +0,0 @@
-package tc.pp3.interfaces;
-
-/**
- * @author tylercambron
- *
- */
-public interface Swimmable {
-	public void swim(int direction);
-}
--- src/tc/pp3/interfaces/Walkable.java
+++
@@ -1,9 +0,0 @@
-package tc.pp3.interfaces;
-
-/**
- * @author tylercambron
- *
- */
-public interface Walkable {
-	public void walk(int direction);
-}
